a82d28fb573f788032cdc8719d825552b9aec27c
Helikopter863 <159429130+Helikopter863@users.noreply.github.com>
2024-02-08 11:14:29 +0100

    Add files via upload

---
+++ chroot-script.sh
@@ -0,0 +1,70 @@
+#!bin/sh
+
+ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
+hwclock --systohc
+sed -i "/^#en_US.UTF-8/ cen_US.UTF-8 UTF-8" /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo archlinux >> /etc/hostname
+
+pacman --noconfirm --needed -S neovim base-devel git networkmanager intel-ucode
+systemctl enable NetworkManager
+systemctl enable systemd-resolved
+
+bootctl install
+mkdir -p /boot/loader/entries /etc/pacman.d/hooks
+
+echo "default  arch.conf 
+timeout  4 
+console-mode max 
+editor  yes" > /boot/loader/loader.conf
+ 
+echo "title  Arch Linux 
+linux  /vmlinuz-linux-zen
+initrd  /intel-ucode.img
+initrd  /initramfs-linux-zen.img
+options  root=/dev/nvme0n1p2 rw
+options  mitigations=off nvidia-drm.modeset=1" > /boot/loader/entries/arch.conf
+
+echo "title  Arch Linux Fallback 
+linux  /vmlinuz-linux-zen
+initrd  /intel-ucode.img
+initrd  /initramfs-linux-zen-fallback.img
+options  root=/dev/nvme0n1p2 rw
+options. mitigations=off nvidia-drm.modeset=1" > /boot/loader/entries/arch-fallback.conf
+
+echo "[Trigger]
+Type = Package
+Operation = Upgrade
+Target = systemd
+
+[Action]
+Description = Gracefully upgrading systemd-boot...
+When = PostTransaction
+Exec = /usr/bin/systemctl restart systemd-boot-update.service" > /etc/pacman.d/hooks/95-systemd-boot.hook
+
+if ! [ $rootpw ]
+then
+  rootpw="root"
+fi
+if ! [ $username ]
+then
+  username="user"
+fi
+if ! [ $userpw ]
+then
+  userpw="user"
+fi
+
+echo "root:$rootpw" | chpasswd
+useradd -mg wheel $username
+echo "$username:$userpw" | chpasswd
+sed -i "/^# %wheel ALL=(ALL:ALL) ALL/ c%wheel ALL=(ALL:ALL) ALL" /etc/sudoers
+
+sed -i "/^#Color/cColor" /etc/pacman.conf
+sed -i "/^#ParallelDownloads/cParallelDownloads = 5" /etc/pacman.conf
+sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+sudo pacman -Syu
+
+pacman --noconfirm --needed -S xorg xorg-xinit noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra pipewire lib32-pipewire wireplumber pipewire-alsa pipewire-pulse pipewire-jack pulsemixer lib32-libglvnd lib32-nvidia-utils lib32-vulkan-icd-loader libglvnd nvidia-dkms nvidia-settings vulkan-icd-loader ttf-jetbrains-mono-nerd maim xclip libxft rofi thunar gvfs htop mpv feh neofetch zip unzip clang
+sudo mkinitcpio -P
---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkfs.xfs -f /dev/nvme0n1p2 || exit && mount /dev/nvme0n1p2 /mnt && mkdir /mnt/boot 
+mkfs.fat -F32 /dev/nvme0n1p1 || exit && mount /dev/nvme0n1p1 /mnt/boot
+mount --mkdir /dev/sda1 /mnt/media/data || exit
+
+pacstrap /mnt base linux-zen linux-zen-headers linux-firmware || exit
+timedatectl set-ntp true
+genfstab -U /mnt >> /mnt/etc/fstab
+curl -L https://raw.githubusercontent.com/Helikopter862/archinstall/main/chroot-script.sh -o /mnt/chroot-script.sh
+arch-chroot /mnt sh chroot-script.sh
+
+reboot

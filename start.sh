0eb7a1da028ef2b3e8cdaa7dd1fd9eb6a0f196a4
Helikopter863 <159429130+Helikopter863@users.noreply.github.com>
2024-02-17 12:28:20 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ mount --mkdir /dev/sda1 /mnt/media/data || exit
 pacstrap /mnt base linux-zen linux-zen-headers linux-firmware || exit
 timedatectl set-ntp true
 genfstab -U /mnt >> /mnt/etc/fstab
-curl -L https://raw.githubusercontent.com/Helikopter862/archinstall/main/chroot-script.sh -o /mnt/chroot-script.sh
+curl -L https://raw.githubusercontent.com/Helikopter863/archinstall/main/chroot-script.sh -o /mnt/chroot-script.sh
 arch-chroot /mnt sh chroot-script.sh
 
 reboot
